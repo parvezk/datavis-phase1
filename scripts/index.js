b77977d22570c07d2b1a03e5598a0d3d091b746d
Parvez Kose <flamboyanz@gmail.com>
2018-01-31 22:31:45 -0500

    update script and styles

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,118 @@
+// Load data & callbacks
+const source = "rows.json";
+d3.json(source, (error, data) => {
+
+	dataset = MergeData(data);
+	//console.log(dataset);
+	addDots();
+	addLineGraph();
+	addLabels();
+	buildAxis();
+});
+
+const statesdata = "states.json";
+let mergeddata,
+	state,
+	key = [];
+
+const MergeData = data => {
+	
+	d3.json(statesdata, (error, states) => {
+		//console.log(data.data)
+		data.data.map((obj, i) => {
+
+			obj["rate"] = obj.forEach((j, k) => { return obj[11] } )
+			obj["state"] = states[i].state;
+			obj["population"] = states[i].population;
+		});
+
+	});
+	return data;
+};
+
+const buttons = document.querySelectorAll("a");
+buttons.forEach(button => {
+	//button.addEventListener("click", showGraph, false);
+	//button.addEventListener("click", (e) => { this.onClick(e); });
+	button.addEventListener("click", function(e) {
+		e.preventDefault();
+		const type = this.className;
+		switchViews(type);
+	});
+});
+
+// Chart
 const h = 420;
 const w = 840;
+const haxis = 80;
+const vaxis = 25;
 let dataset;
-let showrates = false;
+let showrates = true;
+
+const switchViews = type => {
+	//svg.selectAll("*").remove();
+	if (type == "line") {
+		svg.selectAll("circle").remove();
+		addLineGraph();
+		addLabels();
+	} else if (type == "nodes") {
+		// svg.selectAll("path.line-graph").remove();
+		// add dots
+		addDots();
+		addLabels();
+	} else if (type == "both") {
+		addDots();
+		addLineGraph();
+		addLabels();
+	} else if ((type = "rates")) {
+
+		showrates = showrates ? false : true;
+		
+		if (showrates) {
+			
+			console.log("ON")
+			addLabels();
+			showrates = true;
+			
+		} else {
+			console.log("OFF")
+			svg.selectAll(".labels").remove();
+			showrates = false;
+		}
+
+	} // end of if
+}; // end of buildLines
 
 const svg = d3.select(".container svg");
 svg.attrs({ width: w, height: h }).style("background", "oldlace");
 
-const lineG = svg.append("g");
-const nodeG = svg.append("g");
+var div = d3
+	.select("body")
+	.append("div")
+	.attr("class", "tooltip")
+	.style("opacity", 0);
+
+const lineG = svg.append("g").attr("class", "lineG")
+const nodeG = svg.append("g").attr("class", "nodeG")
 
 const addLineGraph = () => {
 	lineG.style("background", "oldlace");
 	const lineFun = d3
 		.line()
 		.x(function(d) {
-			const x = d.crime_index_ranking * 80;
+			const x = d[2] * haxis;
 			return Math.trunc(x);
 		})
 		.y(function(d) {
 			//console.log(d.rate)
-			const y = h - Math.trunc(d.rate) / 25;
+			const y = h - Math.trunc(d[11]) / vaxis;
 			return Math.trunc(y);
 		})
 		.curve(d3.curveLinear);
 
 	const viz = lineG.append("path").attrs({
 		class: "line-graph",
-		d: lineFun(dataset),
+		d: lineFun(dataset.data),
 		stroke: "cadetblue",
 		"stroke-width": 2,
 		opacity: 1,
@@ -36,57 +122,89 @@ const addLineGraph = () => {
 };
 
 const addDots = () => {
+	let i = 1;
 	nodeG.style("background", "lemonchiffon");
 	nodeG
 		.selectAll("circle")
-		.data(dataset)
+		.data(dataset.data)
 		.enter()
 		.append("circle")
 		.attrs({
 			cx: d => {
-				const x = d.crime_index_ranking * 80;
+				const x = d[2] * haxis;
 				return Math.trunc(x);
 			},
 			cy: d => {
-				const y = h - Math.trunc(d.rate) / 25;
+				console.log(d)
+				const y = h - Math.trunc(d[11]) / vaxis;
 				return Math.trunc(y);
 			},
 			r: 5,
 			opacity: 1,
-			fill: d => salesKPI(d.rate),
+			fill: d => salesKPI(d[11]),
 			// 'fill': 'cornflowerblue',
 			transform: "translate(0, 0)"
+		})
+		.style("cursor", "pointer")
+		.on("mouseover", function(d) {
+			console.log(d);
+			div
+				.transition()
+				.duration(200)
+				.style("opacity", 0.9);
+			div
+				.html(
+					"<article>" +
+						"<p>" +
+						"Crime Index Rating: <span>" +
+						d.crime_index_ranking +
+						"</span></p>" +
+						//"<p>" + "State: " + d.state + "</p>" +
+						"<p>" +
+						"Population: <span>" +
+						d.population +
+						"</span></p>" +
+						"</article>"
+				)
+				.style("left", d3.event.pageX + 5 + "px")
+				.style("top", d3.event.pageY - 28 + "px");
+		})
+		.on("mouseout", function(d) {
+			div
+				.transition()
+				.duration(500)
+				.style("opacity", 0);
 		});
+	i++;
 };
 
-const buildLine = type => {
-	//svg.selectAll("*").remove();
-
-	if (type == "line") {
-		svg.selectAll("circle").remove();
-		addLineGraph();
-	} else if (type == "scatter") {
-		svg.selectAll("path.line-graph").remove();
-		// add dots
-		addDots();
-	} else if (type == "both") {
-
-		addDots();
-		addLineGraph();
-	} else if ((type = "toggle")) {
-		showrates = showrates ? false : true;
-		console.log(showrates);
-		svg.selectAll("g").remove();
-		if (showrates) {
-			addLabels();
-			buildAxis();
-		} else {
-			svg.selectAll("text").remove();
-			buildAxis();
-		}
-	} else {
-	} // end of if
-}; // end of buildLines
+const addLabels = () => {
+	// add labels
+	const labels = nodeG
+		.selectAll("text")
+		.data(dataset)
+		.enter()
+		.append("text")
+		.text(d => {
+			console.log(d)
+			return Math.trunc(d[11]);
+		})
+		.attrs({
+			class: "labels",
+			x: d => {
+				const x = d[2] * haxis - 15;
+				return Math.trunc(x);
+			},
+			y: d => {
+				const y = h - Math.trunc(d[11]) / vaxis - 10;
+				return Math.trunc(y);
+			},
+			"font-family": "sans-serif",
+			"font-size": "12px",
+			fill: "#666666",
+			"text-anchor": "start"
+		});
+};
 
 const buildAxis = () => {
 	// defining scales with their domain and range
@@ -102,14 +220,14 @@ const buildAxis = () => {
 		.nice();
 
 	var ticks = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
+	
 	var xAxis = d3
 		.axisBottom()
 		.scale(xScale)
 		.tickValues(ticks)
 		.tickFormat(function(d, i) {
-			//console.log(i, dataset[i].city)
-			return dataset[i].city;
+			//console.log(i, Math.ceil(parseFloat(dataset.data[i][11])))
+			return Math.ceil(parseFloat(dataset.data[i][11]));
 		});
 
 	var yAxis = d3
@@ -126,53 +244,18 @@ const buildAxis = () => {
 	svg
 		.append("g")
 		.attr("class", "axisY")
-		.attr("transform", "translate(40, -7)")
+		.attr("transform", "translate(45, -7)")
 		.call(yAxis);
 };
-
-const addLabels = () => {
-	console.log("comes here");
-	// add labels
-	const labels = svg
-		.selectAll("text")
-		.data(dataset)
-		.enter()
-		.append("text")
-		.text(d => {
-			return Math.trunc(d.rate);
-		})
-		.attrs({
-			class: "circles",
-			x: d => {
-				const x = d.crime_index_ranking * 80 - 15;
-				return Math.trunc(x);
-			},
-			y: d => {
-				const y = h - Math.trunc(d.rate) / 25 - 10;
-				return Math.trunc(y);
-			},
-			"font-family": "sans-serif",
-			"font-size": "12px",
-			fill: "#666666",
-			"text-anchor": "start"
-		});
-};
-
-//const path = "https://data.cityofnewyork.us/resource/h6yn-47fn.json";
-const path = "./../rows.json";
-d3.json(path, function(error, data) {
-	dataset = data;
-	//console.log(data)
-	addDots();
-	addLineGraph();
-	addLabels();
-	buildAxis();
-});
 
 // KPI #666666; #33CC66
 const salesKPI = d => {
 	if (d >= 4800) {
-		return "red";
+		//return "#D17878";
+		//return "#FF8080";
+		//return "#FF9585";
+		return "cornflowerblue";
+		
 	} else if (d <= 4800) {
 		return "cornflowerblue";
 	}
@@ -191,28 +274,3 @@ const showMinMax = (ds, col, val, type) => {
 		}
 	}
 };
-
-const buttons = document.querySelectorAll("a");
-
-buttons.forEach(button => {
-	//button.addEventListener("click", showGraph, false);
-	//button.addEventListener("click", (e) => { this.onClick(e); });
-	button.addEventListener("click", function(e) {
-		e.preventDefault();
-		const type = this.className;
-		buildLine(type);
-	});
-});
-
-const showGraph = e => {
-	e.preventDefault();
-	console.log("ll");
-};
-
-/*
-buttons.onclick((e) => {
-	e.preventDefault();
-	const button = e.attr('class');
-	console.log(button);
-})
-*/
